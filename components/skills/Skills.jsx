f99adeeb5821f88e2304b91a9fe3c1b545be63fa
Ayush-Dangol <dangolice@gmail.com>
2023-11-25 23:36:02 -0500

    Update Skills.jsx

--- components/skills/Skills.jsx
+++ components/skills/Skills.jsx
@@ -20,16 +20,19 @@ const Skills = () => {
     <div>
       <h1 className="text-7xl text-center py-2 pb-5 mb-10 heading">Skills</h1>
       <div className={styles.skills}>
-        {skillList.map((item) => {
+        {skillList.map((item, index) => {
           return (
-            <span className="flex flex-col justify-center items-center cursor-pointer w-60">
+            <span
+              key={index}
+              className="flex flex-col justify-center items-center cursor-pointer w-60"
+            >
               <div className={styles.imgContainer}>
                 <Image
                   src={`/icons/${item}.png`}
                   fill
                   className={`grayscale hover:grayscale-0 `}
                   id={item}
-                  alt="skills"
+                  alt={`Logo for ${item} skill`}
                 />
               </div>
 
